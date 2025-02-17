856a5c53b1bb751dc1abe364a48d205fe3a2550e
cmd-sachin <sachiniyyappan2005@gmail.com>
2025-02-17 16:31:16 +0530

    Base ID changed

--- src/app/api/airTable/route.js
+++ src/app/api/airTable/route.js
@@ -3,9 +3,8 @@ import Airtable from "airtable";
 export async function GET() {
   try {
     const base = new Airtable({
-      apiKey:
-        "patyfDcedyFIgbnAE.d453878862966d0a8e6e210e2a57b2056aa6ce62f8d96ea597fc3d033b5a678e",
-    }).base("appBZJKmKN3iViICl");
+      apiKey: process.env.accessTokenKey,
+    }).base(process.env.baseKey);
     let allRecords = [];
     const records = await new Promise((resolve, reject) => {
       const tempRecords = [];
